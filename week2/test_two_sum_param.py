fd63c53c95e0208b2d10207079b81dae9914a724
mthier <ThierschMarkus@VSFTFA0107-ML01.local>
2025-07-25 17:08:19 +0200

    day8: refactor + parametrised pytest (6 green)

--- week2/test_two_sum_param.py
+++ week2/test_two_sum_param.py
@@ -1,4 +1,5 @@
-import pytest
+import pytest #Today we use @pytest.mark.parametrize, which is defined inside the pytest library,
+#so we must import pytest to tell Python where that decorator lives.
 from day5_twosum import two_sum
 
 @pytest.mark.parametrize(
@@ -7,7 +8,7 @@ from day5_twosum import two_sum
         ([2, 7, 11, 15], 9, [0, 1]),
         ([3, 2, 4],     6, [1, 2]),
         ([0, 4, 3, 0],  0, [0, 3]),
-        ([],             5, []),
+        ([],             5, []), #[] in "target" possion means no pair possible, which is true (will pass the test)
         ([5],            5, []),
         ([-1, -2, -3], -3, [0, 1])
     ]
